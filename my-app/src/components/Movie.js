b9b7cb5ec319d8d118bbe446bb39cf803d3f5668
Maria Trujillo <mptruj015@gmail.com>
2021-01-12 17:35:39 +0100

    added movie card component

---
+++ my-app/src/components/Movie.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Movie = ({movie}) => {
+
+
+    return (
+        <div className="movie-wrapper">
+            <div className="movie-card">
+                <h2>{movie.Title}</h2>
+                <div className="movie-poster">
+                    <img 
+                        width="200"
+                        alt={`Movie title: ${movie.Title}`}
+                        src={movie.Poster}
+                    />
+                </div>
+                <p className="movie-year">
+                    Year: {movie.Year}
+                </p>
+
+            </div>
+            <div className="nominate-button">
+                Nominate
+            </div>
+        </div>
+    )
+}
+
+export default Movie;
