6e6fc9a92a49c6a127cee28ab156ed5c4e3d200b
Maria Trujillo <mptruj015@gmail.com>
2021-01-15 20:56:35 +0100

    added reusable button

---
+++ my-app/src/components/NominateButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NominateButton = (props) => {
+    return (
+        <div className="nominate-button">
+            {props.text}
+        </div>
+    )
+}
+
+export default NominateButton;
