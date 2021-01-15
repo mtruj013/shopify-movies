93b978b92f667769808edc384cc39ceb0d48c27d
Maria Trujillo <mptruj015@gmail.com>
2021-01-15 20:57:17 +0100

    added component for nomnated movies

--- my-app/src/components/SavedList.js
+++ my-app/src/components/SavedList.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import '../styles/MovieCard.scss';
+import NominateButton from './NominateButton';
 
-const SavedList = () => {
- return (
-     <div></div>
- )   
+const SavedList = (props) => {
+    const movie = props.movie
+
+    return (
+        // <div className="saved-container">
+            <div className="movie-wrapper">
+                <div className="movie-card">
+                    <h2>{movie.Title}</h2>
+                    <div className="movie-poster">
+                        <img
+                            width="200"
+                            alt={`Movie title: ${movie.Title}`}
+                            src={movie.Poster}
+                        />
+                    </div>
+                    <p className="movie-year">
+                        Year: {movie.Year}
+                    </p>
+
+                </div>
+                <div className="delete"> <NominateButton text="Remove"/></div>
+            </div>
+        // </div>
+    )
 }
 
 export default SavedList;
